056874cfacf8c236080bb364d4e56c11d2d550bd
SiddhiMarne-Prabha <siddhi.marne.prabha@gmail.com>
2026-06-17 13:24:25 +0530

    new assignment

---
+++ Assigments/Assignment1.py
@@ -0,0 +1,21 @@
+# Q1
+print("my name is siddhi")
+print("my favourite programming language is python")
+
+
+#Q2
+name=input("enter your name")
+age=int(input("enter your age"))
+print("hello",name,"you are",age,"years old")
+
+
+#Q3
+student_name="siddhi"
+roll_no=42
+percentage=87.60
+passed_status=True
+print("student details:")
+print("name:",student_name)
+print("Roll_no:",roll_no)
+print("percentage:",percentage)
+print("passed:",passed_status)
